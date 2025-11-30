cd2d024388b89f573709b8886d4f47b7e89882a9
alireza <theethanwardhere@gmail.com>
2025-11-30 07:59:18 +0330

    craete statment

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # This is a practice for ORM(hibernate) and JDBC to connect to the data base
+
+
+*DriverManager* : Creates a connection to the database using a URL, username, and password.
+
+*Connection* : Represents the session with the database; used to create statements.
--- src/main/java/com/arcade/jdbc/ConnectionConfig.java
+++ src/main/java/com/arcade/jdbc/ConnectionConfig.java
@@ -1,4 +1,16 @@
 package com.arcade.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class ConnectionConfig {
+    private static final String url = "jdbc:h2:./app;AUTO_SERVER=TRUE";
+    public static void operate() throws SQLException {
+        String createStatement = "CREATE TABLE IF NOT EXIST USER (id IDENTITY PRIMARY KEY , name VARCHAR)";
+        Connection connection = DriverManager.getConnection(url); //Driver manager creates a connection to Driver(DB)
+        Statement statement = connection.createStatement();
+        statement.execute(createStatement);
+    }
 }
